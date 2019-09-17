5ce541d1a252215adf549f3958739ffbcf36c42d
Fcscanf <fcscanf@gmail.com>
2019-09-17 19:37:35 +0800

    [Codes-Fcant-Updates]:安装配置项目所需的组件：vue-resource、axios、element-ui、mint-ui、vuex、moment

--- README.md
+++ README.md
@@ -19,3 +19,24 @@ cnpm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# 项目开发进度笔记 
+-----
+## 1.安装前后端请求工具
+```bash
+$ cnpm i vue-resource -S
+$ cnpm i axios -S
+``` 
+## 2.安装样式组件库
+```bash
+$ cnpm i element-ui -S
+$ cnpm i mint-ui -S
+```
+## 3.安装Vuex
+```bash
+$ cnpm i vuex -S
+```
+## 4.安装时间插件moment
+```bash
+$ cnpm i moment -S
+```
--- src/main.js
+++ src/main.js
@@ -8,12 +8,32 @@ import VueResource from 'vue-resource'
 import axios from 'axios'
 import Qs from 'qs'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+import moment from 'moment'
+
+import Vuex from 'vuex'
+Vue.use(Vuex)
+Vue.use(moment)
+Vue.use(MintUI)
 Vue.use(VueResource)
+Vue.use(ElementUI);
 
 Vue.prototype.axios = axios;
 Vue.prototype.qs = Qs;
 
 Vue.http.options.root = 'http://www.fcant.com:8080'
+
+// 配置全局过滤器
+Vue.filter('dateFormat', function (dataStr, pattern = 'YYYY-MM-DD HH:mm:ss') {
+  return moment(dataStr).format(pattern)
+})
+// 全局设置post请求的数据格式
+Vue.http.options.emulateJSON = true
 
 Vue.config.productionTip = false
 
